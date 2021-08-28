d269881a023389277e0b0d7a909c9d88a4354c28
Caius Durling <dev@caius.name>
2021-08-28 21:51:43 +0100

    100 blocks, coming right up

---
+++ generate.rb
@@ -0,0 +1,33 @@
+require "erb"
+
+puts ERB.new(DATA.read, 0, "-<>").result(self.__send__(:binding))
+
+__END__
+version: "v1.0"
+name: "Run things"
+agent:
+  machine:
+    type: e1-standard-2
+    os_image: ubuntu1804
+
+blocks:
+<% 99.times do |i| %>
+  - name: "Block <%= i + 1 %>"
+    dependencies: []
+    task:
+      jobs:
+      - name: "Job 1"
+        commands:
+          - "true"
+<% end %>
+
+  - name: "Finish"
+    dependencies:
+<% 99.times do |i| -%>
+      - "Block <%= i + 1 %>"
+<% end -%>
+    task:
+      jobs:
+        - name: "Fin"
+          commands:
+            - "true"
